67593829a1815d5d4cc6e09e35bdebc3e8ade0ba
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-07-13 00:20:40 +0300

    featApi:updates on the files

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 import requests
+import os
 from flask import Flask, jsonify #fot outputing json from the api
-from flask_cors import CORS#for connection to frontend 
+from flask_cors import CORS #for connection to frontend 
 app = Flask(__name__)
 
 CORS(app) 
@@ -47,7 +48,10 @@ def myAge():
     age = 25
     return jsonify(age)     
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # use Render's port or fallback to 5000
+    app.run(debug=True, host="0.0.0.0", port=port)
+
+    
 
 
 
